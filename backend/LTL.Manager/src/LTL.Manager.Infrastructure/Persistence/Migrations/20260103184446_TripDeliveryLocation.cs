dbdd4cfdb08c5f6a55c1ad6cb5c4e4e9797f5bb5
Kytoon <eryk.olsza@gmail.com>
2026-01-03 20:12:10 +0100

    migrations added

---
+++ backend/LTL.Manager/src/LTL.Manager.Infrastructure/Persistence/Migrations/20260103184446_TripDeliveryLocation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LTL.Manager.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TripDeliveryLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryLocation",
+                table: "Trips",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Trips",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryLocation",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Trips");
+        }
+    }
+}
---
+++ backend/LTL.Manager/src/LTL.Manager.Infrastructure/Persistence/Migrations/20260103191028_TripCofigurationAdded.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LTL.Manager.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TripCofigurationAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Trips",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeliveryLocation",
+                table: "Trips",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Trips",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Trips",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "getdate()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeliveryLocation",
+                table: "Trips",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Trips",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "getdate()");
+        }
+    }
+}
